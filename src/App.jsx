78e2fea7b5e138579d2fd870248fac905db824a7
Robin Punnoose <robin.punnoose@protonmail.com>
2023-02-15 21:26:05 -0600

    added weatherservice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
 import "./App.css";
 import Cities from "./components/Cities/Cities";
 import Forecast from "./components/Forecast/Forecast";
 import Inputs from "./components/Inputs/Inputs";
 import TempAndDetails from "./components/TempAndDetails/TempAndDetails";
 import TimeAndLocation from "./components/TimeAndLocation/TimeAndLocation";
+import getWeatherData from "./services/weatherService";
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getWeatherData("weather", { q: "Chicago" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <div className="App">
       <Cities />
---
+++ src/services/weatherService.js
@@ -0,0 +1,13 @@
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(import.meta.env.VITE_BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({
+    ...searchParams,
+    appid: import.meta.env.VITE_API_KEY,
+  });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
+
+export default getWeatherData;
